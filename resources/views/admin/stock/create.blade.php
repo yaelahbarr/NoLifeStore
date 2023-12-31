5b3ba7747f39395e51989584cddef25611201695
diosahmadfadhil1 <kakatua08542@gmail.com>
2023-12-31 23:11:47 +0700

    Memperbaiki semua form

--- resources/views/admin/stock/create.blade.php
+++ resources/views/admin/stock/create.blade.php
@@ -1,14 +1,15 @@
 @extends('admin.layouts.main')
 
 @section('container')
-<div class="container mx-auto mt-8">
+<br>
+<div class="container mx-start max-w-md mt-9">
     <h1 class="text-2xl font-semibold mb-2 bg-gray-800 text-white py-2 px-8 rounded text-center ">Add New Product</h1>
 
         <form action="{{ route('stock.store') }}" method="post" enctype="multipart/form-data">
             @csrf
 
             <!-- Kategori -->
-            <div class="mb-4 mx-start max-w-md">
+            <div class="mb-4">
                 <label for="category" class="block text-sm font-medium text-gray-700">Category</label>
                 <select name="id_category" id="category" class="mt-1 block w-full border-gray-300 rounded-md focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                     @foreach ($category as $c)
@@ -18,31 +19,31 @@
             </div>
 
             <!-- Nama Produk -->
-            <div class="mb-4 mx-start max-w-md">
+            <div class="mb-4">
                 <label for="name" class="block text-sm font-medium text-gray-700">Product Name</label>
                 <input type="text" name="product_name" id="name" class="mt-1 block w-full border-gray-300 rounded-md focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
             </div>
 
             <!-- Harga -->
-            <div class="mb-4 mx-start max-w-md">
+            <div class="mb-4">
                 <label for="price" class="block text-sm font-medium text-gray-700">Price</label>
                 <input type="number" name="price" id="price" class="mt-1 block w-full border-gray-300 rounded-md focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
             </div>
 
             <!-- Stok -->
-            <div class="mb-4 mx-start max-w-md">
+            <div class="mb-4">
                 <label for="stock" class="block text-sm font-medium text-gray-700">Stock</label>
                 <input type="number" name="stock" id="stock" class="mt-1 block w-full border-gray-300 rounded-md focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
             </div>
 
             <!-- Gambar -->
-            <div class="mb-4 mx-start max-w-md">
+            <div class="mb-4">
                 <label for="image" class="block text-sm font-medium text-gray-700">Image</label>
                 <input type="file" name="image" id="image" class="mt-1 block w-full border-gray-300 rounded-md focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
             </div>
 
             <!-- Tombol Submit -->
-            <div class="mb-4 mx-start max-w-md">
+            <div class="mb-4">
                 <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">Submit</button>
             </div>
         </form>
